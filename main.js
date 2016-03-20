56ae8cd0d1dd3d37a22d2917cedfabd99fa5a311
Doelia <kyxalia@gmail.com>
2016-03-20 16:51:33 +0100

    v1.1 : correction du clignotement

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 var colors = require('colors');
+var util = require('util');
+var exec = require('child_process').exec;
 
 class Dactylo {
 
 
     constructor() {
         this.text = `
-        On parle de suggestion. C'en est là de la plus naturelle et de la plus croyable.
-        Je me méfie un peu, malgré tout, de celle qui est préparée dans les cliniques.
-        Mais qu'un être chez qui la volonté est morte obéisse à toutes les excitations extérieures,
-        c'est une vérité que la raison admet et que démontre l'expérience. L'exemple que vous
-        en apportez m'en rappelle un autre assez analogue. C'est celui de mon malheureux camarade
-        Alexandre Le Mansel. Un vers de Sophocle tua votre héroïne. Une phrase de Lampride perdit l'ami...
+On parle de suggestion. C'en est là de la plus naturelle et de la plus croyable.
+Je me méfie un peu, malgré tout, de celle qui est préparée dans les cliniques.
+Mais qu'un être chez qui la volonté est morte obéisse à toutes les excitations extérieures,
+c'est une vérité que la raison admet et que démontre l'expérience. L'exemple que vous
+en apportez m'en rappelle un autre assez analogue. C'est celui de mon malheureux camarade
+Alexandre Le Mansel. Un vers de Sophocle tua votre héroïne. Une phrase de Lampride perdit l'ami...
         `
 
         this.cursor = 0;
@@ -48,14 +50,23 @@ class Dactylo {
     }
 
     displayText() {
+        'use strict';
+        var out = "";
+
+        process.stdout.write('\x1B[2J');
         for (var i = 0; i < this.text.length; i++) {
             var c = this.text[i];
             if (i < this.cursor) {
-                process.stdout.write(c.green);
+                out += c.green;
             } else {
-                process.stdout.write(c);
+                out += c;
             }
         }
+
+        out += '\n\nActual char : '+this.cursor;
+        out += '\n';
+
+        console.log(out);
     }
 }
 
